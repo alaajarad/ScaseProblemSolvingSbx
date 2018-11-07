66838041cb6f44a738341955c5cddf01d64d6fb4
unknown <user@pc-119.scase-tar.com>
2018-11-07 12:39:00 +0200

    Gears problem solution

--- Problems/Gears/src/gears.cpp
+++ Problems/Gears/src/gears.cpp
@@ -1,8 +1,18 @@
 #include "gears.h"
+#include <vector>
 
 
 int Gears::GetNumberOfDistinctPairs(const std::vector<int>& gears)
 {
   // Your code goes here !!!
+	int n = gears.size();
+	if (n == 1)
+		return 0;
+	int result = 0;
+	for (int i = 0; i <= n - 2; ++i)
+		for (int j = i + 1; j < n; ++j)
+			if (gears[i] % gears[j] == 0 || gears[j] % gears[i] == 0)
+				++result;
+	return result;
   return 0;
 }
