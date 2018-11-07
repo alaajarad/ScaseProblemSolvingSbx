cac8a930528be02aa832c393f583da00722eab77
Mohammad Nasser <mohammad.nasser@lit-co.net>
2018-11-07 11:50:35 +0200

    calculate maximum time required for a test case

--- Tests/Problems/test_gears.cpp
+++ Tests/Problems/test_gears.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <fstream>
 #include <iostream>
 
@@ -13,6 +14,10 @@ TEST(Subtraction, Subtraction)
   }
   int numberOfTestCases;
   inf >> numberOfTestCases;
+
+  // maximum time use for a test case
+  clock_t maxTimeUsed = 0.0;
+
   for (int i = 0; i < numberOfTestCases; i++)
   {
     int nOfGears;
@@ -26,6 +31,15 @@ TEST(Subtraction, Subtraction)
     }
     int expectedAnswer;
     outf >> expectedAnswer;
+	
+	clock_t start = std::clock();
+
     EXPECT_EQ(Gears::GetNumberOfDistinctPairs(gears), expectedAnswer);
+
+	//calculate the maximum time used for a test case
+	clock_t end = std::clock();
+	clock_t timeUsed = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+	maxTimeUsed = std::max(timeUsed, maxTimeUsed);
   }
+  std::cout << "Maximum time required for a test case " << maxTimeUsed << " ms\n";
 }
