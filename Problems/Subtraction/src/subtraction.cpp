b2307e1eee18016f6e205524904b82dfce1e6fda
AshrafAljerdy <ashraf.aljerdy@lit-co.net>
2018-11-07 00:13:34 -0800

    solved problem Subtraction and calculated the most time-consuming test

--- Problems/Subtraction/src/subtraction.cpp
+++ Problems/Subtraction/src/subtraction.cpp
@@ -2,6 +2,23 @@
 
 int Subtraction::GetMaxNumber(const std::vector<int>& sequence)
 {
-  // Your code goes here !!!
-  return 0;
+	int sz = sequence.size();
+	
+	if (sz == 0)
+		return 0;
+
+	int res = sequence[0];
+	int tmp = 0;
+	bool ok = 1;
+	for (int i = 1; i < sz; i++)
+	{
+		if (ok)
+		{
+			tmp = sequence[i];
+			ok = 0;
+			continue;
+		}
+		tmp -= sequence[i];
+	}
+  return (res - (tmp));
 }
