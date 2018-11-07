04ef5d494d19e1e4c38a2ab42f8ce37d6d6726db
unknown <user@pc-119.scase-tar.com>
2018-11-07 15:43:46 +0200

    Adding Subtraction problem solution

--- Problems/Subtraction/src/subtraction.cpp
+++ Problems/Subtraction/src/subtraction.cpp
@@ -1,7 +1,22 @@
 #include "subtraction.h"
+#include <vector>
+
+using namespace std;
 
 int Subtraction::GetMaxNumber(const std::vector<int>& sequence)
 {
   // Your code goes here !!!
-  return 0;
+
+	int n = sequence.size();
+	if (n == 1)
+		return sequence[0];
+	else if (n == 2)
+		return sequence[0] - sequence[1];
+	else
+	{
+		int result = sequence[0] - sequence[1];
+		for (int idx = 2; idx < n; ++idx)
+			result += sequence[idx];
+		return result;
+	}
 }
