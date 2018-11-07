eb4332e62e6e817234e32c5d7388eb388302f1f5
unknown <user@pc-116.scase-tar.com>
2018-11-07 15:56:03 +0200

    Solving and Testing  sample problem ...

--- Tests/Problems/test_sample.cpp
+++ Tests/Problems/test_sample.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include <iostream>
-
+#include <ctime>
 #include "sample.h"
 
 TEST(Sample, Sample)
@@ -11,14 +11,25 @@ TEST(Sample, Sample)
   {
     FAIL() << "Error opening test file";
   }
+  double maxExecutionTime = 0;
+  double executionTime;
   int numberOfTestCases;
   inf >> numberOfTestCases;
   for (int i = 0; i < numberOfTestCases; i++)
-  {
+  { 
     int lhs, rhs;
     inf >> lhs >> rhs;
     int expectedAnswer;
     outf >> expectedAnswer;
+	//time 
+	int start_s = clock();
     EXPECT_EQ(Sample::AddNumbers(lhs, rhs), expectedAnswer);
+	int stop_s = clock();
+	 executionTime = (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
+	 maxExecutionTime = std::max(maxExecutionTime, executionTime);
+	
+
+	//end
   }
+  std::cout << " The longest execution time for tests in ms is :  " << maxExecutionTime <<"\n";
 }
