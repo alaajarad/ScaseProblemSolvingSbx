98867fa3bb289ee0268438876f59bdfcb7923942
Arwa Fahoud <arwa.fahoud@lit-co.net>
2018-11-08 15:05:10 +0200

    end of task

--- Problems/Subtraction/src/main.cpp
+++ Problems/Subtraction/src/main.cpp
@@ -1,7 +1,24 @@
 #include "subtraction.h"
+#include<iostream>
+using namespace std;
 
 int main()
 {
- 
+	int t, n;
+	int sub[100];
+	std::vector<int> num;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n;
+		for (int j = 0;j < n;j++)
+		{
+			cin >> num[j];
+		}
+		sub[i] = Subtraction::GetMaxNumber(num);
+	}
+	for (int i = 0;i < t;i++)
+		cout << sub[i] << endl;
+
   return 0;
 }
