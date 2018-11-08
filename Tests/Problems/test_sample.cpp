5dec235fc2d879922fdad17883a92268e0003d25
Arwa Fahoud <arwa.fahoud@lit-co.net>
2018-11-08 14:45:53 +0200

    correcting test file

--- Tests/Problems/test_sample.cpp
+++ Tests/Problems/test_sample.cpp
@@ -23,8 +23,8 @@ TEST(Sample, Sample)
     int start = clock();
     EXPECT_EQ(Sample::AddNumbers(lhs, rhs), expectedAnswer);
     int stop = clock();
-    int elapsed = (stop-start)/double(CLOCKS-PER-SEC)*1000;
+    double elapsed = (stop-start)/double(CLOCKS_PER_SEC)*1000;
     max1 = std::max(max1,elapsed);
   }
-  std::cout<<"max elapsed time"<< max1;
+  std::cout<<"max elapsed time "<< max1<< std::endl;
 }
