b4206e2c2d23a39cf7e46b688e59bd2631b73133
Arwa Fahoud <arwa.fahoud@lit-co.net>
2018-11-08 14:25:49 +0200

    calculating the max elapsed time

--- Tests/Problems/test_sample.cpp
+++ Tests/Problems/test_sample.cpp
@@ -5,6 +5,7 @@
 
 TEST(Sample, Sample)
 {
+    int max1=0;
   std::ifstream inf("sample.in");
   std::ifstream outf("sample.out");
   if (!inf.is_open() || !outf.is_open())
@@ -19,6 +20,11 @@ TEST(Sample, Sample)
     inf >> lhs >> rhs;
     int expectedAnswer;
     outf >> expectedAnswer;
+    int start = clock();
     EXPECT_EQ(Sample::AddNumbers(lhs, rhs), expectedAnswer);
+    int stop = clock();
+    int elapsed = (stop-start)/double(CLOCKS-PER-SEC)*1000;
+    max1 = std::max(max1,elapsed);
   }
+  std::cout<<"max elapsed time"<< elapsed;
 }
